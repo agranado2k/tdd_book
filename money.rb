f81a82f2eb59156a10ff5174a2734bce1aabe8b6
Arthur Granado <agranado2k@gmail.com>
2019-01-23 23:54:39 +0000

    Chapter 1

---
+++ money.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+
+class Dollar
+  attr_reader :amount
+
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def times(mult)
+    self.class.new(@amount * mult)
+  end
+end
+
+describe Dollar do
+  it 'multipli amount' do
+    five = Dollar.new(5)
+
+    ten = five.times(2)
+
+    expect(ten.amount).to eq(10)
+  end
+end
+
+
+## Test list
+# $5 + 10 CHF = $10 if rate is 2:1
+# done ==> $5 * 2 = $10
+# Money rounding?
