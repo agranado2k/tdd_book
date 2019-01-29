df75317f55c219d97c2517fd251eadff8b243fcb
Arthur Granado <agranado2k@gmail.com>
2019-01-29 21:48:59 +0000

    Chapter 14

--- money.rb
+++ money.rb
@@ -30,14 +30,28 @@ class Money
     Sum.new(self, addend)
   end
 
-  def reduce(to_currency)
-    self
+  def reduce(bank, to_currency)
+    rate = bank.rate(currency, to_currency)
+    Money.new(amount / rate, to_currency)
   end
 end
 
 class Bank
+  def initialize
+    @rates = {}
+  end
+
   def reduce(exp, to_currency)
-    exp.reduce(to_currency)
+    exp.reduce(self, to_currency)
+  end
+
+  def add_rate(from, to, rate)
+    @rates["#{from}-#{to}"] = rate
+  end
+
+  def rate(from, to)
+    return 1 if from == to
+    @rates["#{from}-#{to}"]
   end
 end
 
@@ -49,7 +63,7 @@ class Sum
     @addend = addend
   end
 
-  def reduce(to_currency)
+  def reduce(bank, to_currency)
     Money.new(augend.amount + addend.amount, to_currency)
   end
 end
@@ -103,6 +117,23 @@ describe Money do
       reduced = bank.reduce(five, 'USD')
       expect(reduced).to be_equal(Money.dollar(5))
     end
+
+    it 'reduce Money with different currencies' do
+      bank = Bank.new()
+      bank.add_rate('CHF', 'USD', 2)
+      money = Money.franc(2)
+      reduced = bank.reduce(money, 'USD')
+      expect(reduced).to be_equal(Money.dollar(1))
+    end
+  end
+end
+
+describe Bank do
+  context 'Convert rates' do
+    it 'Same currency for from and to' do
+      bank = Bank.new
+      expect(bank.rate('USD', 'USD')).to eq(1)
+    end
   end
 end
 
@@ -121,8 +152,8 @@ end
 # done ==> Common times
 # done ==> Compare Francs with Dollars
 # done ==>  Currency?
-# $5 + $5 = $10
+# done ==> $5 + $5 = $10
 # Return Money from  $5 + $5
 # done ==> Bank.reduce(Money)
-# Reduce Money with currency
-# Reduce(Bank, string)
+# done ==> Reduce Money with currency
+# done ==> Reduce(Bank, string)
